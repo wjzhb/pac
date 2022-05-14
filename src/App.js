c47b9aba53f4342fd5db51e48162dbf40f639913
wjzhb <wjzhb@qq.com>
2022-05-14 11:40:49 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Home from './pages/home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                {/* 路由精确匹配"/home"，跳转Home页面 */}
+                <Route exact path="/home" element={<Home />} />
+                {/* 未匹配，则跳转Login页面 */}
+                <Route path="*" element={<Navigate to="/home" />} />
+            </Routes>
+        </HashRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import { Button } from 'antd'
+function Home() {
+
+    return (
+        <div className="P-home">
+            <h1>Home Page</h1>
+            <div className="ipt-con">
+                <Button>返回登录</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
